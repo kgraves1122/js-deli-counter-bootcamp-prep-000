1762997c9d536d90b02606e31b9611cf6f2d98fa
Beanie Pies <kgravesjohnson@gmail.com>
2018-09-11 13:39:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-function takeNumber (lineNumber, name) {
-  var realNumber = lineNumber + 1;
-  return `Welcome, ${name}. You are number ${realNumber} in line.`;
+function takeANumber (lineNumber, name) {
+  lineNumber = lineNumber.push(name);
+   var num = 0;
+   for (var i = 0; i < lineNumber.length; i++) {
+     if (i )
+     num = num + i;
+   }
+  
+  return "Welcome, "+ name + ". You are number " + num +" in line.";
 }
 
-function nowServing 
+function nowServing (num) {
+  if (num.length > 0) {
+    var name = num[0];
+    num.shift();
+    return name;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine (num) {
+  var emp = "The line is currently: "
+  if (num.length > 0) {
+    for (var i = 0; i < num.length; i++) {
+      if (i + 1 === num.length) {
+      emp = emp + (i + 1) +" . " + num[i]
+      } else {
+        emp = emp + (i + 1) +" . " + num[i] +", "
+      }
+      return emp;
+    }
+  } else { 
+    return "The line is currently empty." }
+}
