1e065c853a97e307ac0e175832ffc44ed8b8367e
Beanie Pies <kgravesjohnson@gmail.com>
2018-09-11 13:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function currentLine (num) {
   var emp = "The line is currently: "
   if (num.length > 0) {
     for (var i = 0; i < num.length; i++) {
-      if (i + 1 === num.length) {
-      emp = emp + (i + 1) +" . " + num[i]
+      if (i + 1 < num.length) {
+      emp = emp + (i + 1) +" . " + num[i] +", "
       } else {
-        emp = emp + (i + 1) +" . " + num[i] +", "
+        emp = emp + (i + 1) +" . " + num[i] 
       }
       return emp;
     }
